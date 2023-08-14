a4b788922523ede388b29e895c22c5352a07608a
Rizki Perdana <perdana.rizki16@gmail.com>
2023-08-14 22:51:32 +0700

    setup views

--- NotutaApi/todo/views.py
+++ NotutaApi/todo/views.py
@@ -11,7 +11,31 @@ from rest_framework.decorators import api_view
 
 @api_view(['GET', 'POST'])
 def todo_list(request):
-    pass
+    if request.method == 'POST':
+        todo_data = JSONParser().parse(request)
+        todo_serializer = TodoSerializer(data=todo_data)
+        if todo_serializer.is_valid():
+            todo_serializer.save()
+            return JsonResponse(
+                todo_serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+        return JsonResponse(
+            todo_serializer.errors, 
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    
+    elif request.method == 'GET':
+        todos = TodoModel.objects.all()
+        title = request.GET.get('title', None)
+        
+        if title is not None:
+            todos = todos.filter(title__icontains=title)
+        
+        todo_serializer = TodoSerializer(todos, many=True)
+        return JsonResponse(
+            todo_serializer.data, safe=False
+        )
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
@@ -23,10 +47,39 @@ def todo_detail(request, pk):
             {'message': f'This todo {pk} is not exist!'},
             status=status.HTTP_404_NOT_FOUND
         )
-    
-    pass
 
+    if request.method == 'GET':
+        todo_serializer = TodoSerializer(todo)
+        return JsonResponse(todo_serializer.data)
+    
+    elif request.method == 'PUT':
+        todo_data = JSONParser().parse(request)
+        todo_serializer = TodoSerializer(
+            todo, data=todo_data
+        )
+        if todo_serializer.is_valid():
+            todo_serializer.save()
+            return JsonResponse(todo_serializer.data)
+        return JsonResponse(
+            todo_serializer.errors,
+            status=status.HTTP_404_NOT_FOUND
+        )
+    
+    elif request.method == 'DELETE':
+        todo.delete()
+        return JsonResponse(
+            {'message': 'Todo was deleted successfully'},
+            status=status.HTTP_204_NO_CONTENT
+        )
+    
 
 @api_view(['GET'])
 def todo_list_completed(request):
-    pass
+    todos = TodoModel.objects.filter(completed=True)
+
+    if request.method == 'GET':
+        todo_serializer = TodoSerializer(todos, many=True)
+        return JsonResponse(
+            todo_serializer.data,
+            safe=False
+        )
